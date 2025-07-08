fa386a299a30ea0c37cf3d579afd715273ec4be8
yarik10101 <yataras13@gmail.com>
2025-07-08 14:33:20 +0200

    save01

--- src/main/scala/scala2Advanced/exercises/MySet.scala
+++ src/main/scala/scala2Advanced/exercises/MySet.scala
@@ -1,6 +1,9 @@
 package scala2Advanced.exercises
 
+import scala.annotation.tailrec
+
 trait MySet[A] extends (A => Boolean) {
+
   def contains(elem: A): Boolean
 
   def +(elem: A): MySet[A]
@@ -14,9 +17,25 @@ trait MySet[A] extends (A => Boolean) {
   def filter(predicate: A => Boolean): MySet[A]
 
   def foreach(f: A => Unit): Unit
+
+  def head: A
+
+  def tail: MySet[A]
+
+  def remove(elem: A, acc: MySet[A]): MySet[A]
+
+  def intersectionWith(anotherSet: MySet[A]): MySet[A]
+
+  def differenceWith(anotherSet: MySet[A]): MySet[A]
+
+  def isEmpty: Boolean
+
 }
 
 case class MySetEmpty[A]() extends MySet[A] {
+
+  override def apply(v1: A): Boolean = false
+
   override def contains(elem: A): Boolean = false
 
   override def +(elem: A): MySet[A] = MySetNonEmpty(elem, this)
@@ -30,14 +49,36 @@ case class MySetEmpty[A]() extends MySet[A] {
   override def filter(predicate: A => Boolean): MySet[A] = MySetEmpty()
 
   override def foreach(f: A => Unit): Unit = {}
+
+  override def head: A = throw new Exception("nope")
+
+  override def tail: MySet[A] = this
+
+  override def remove(elem: A, acc: MySet[A]): MySet[A] = this
+
+  override def intersectionWith(anotherSet: MySet[A]): MySet[A] = this
+
+  override def differenceWith(anotherSet: MySet[A]): MySet[A] = anotherSet
+
+  override def isEmpty: Boolean = true
 }
 
 case class MySetNonEmpty[A](head: A, tail: MySet[A]) extends MySet[A] {
-  override def contains(elem: A): Boolean = apply(elem)
 
-  override def +(elem: A): MySet[A] = MySetNonEmpty(elem, this)
+  override def isEmpty: Boolean = true
 
-  override def ++(anotherSet: MySet[A]): MySet[A] = MySetNonEmpty(head, tail + anotherSet)
+  override def apply(v1: A): Boolean = head match {
+    case h if v1 == h => true
+    case _ => tail(v1)
+  }
+
+  override def contains(elem: A): Boolean = this (elem)
+
+  override def +(elem: A): MySet[A] =
+    if (this contains elem) this
+    else MySetNonEmpty(elem, this)
+
+  override def ++(anotherSet: MySet[A]): MySet[A] = tail ++ anotherSet + head
 
   override def map[B](f: A => B): MySet[B] = MySetNonEmpty(f(head), tail.map(f))
 
@@ -53,4 +94,41 @@ case class MySetNonEmpty[A](head: A, tail: MySet[A]) extends MySet[A] {
     tail.foreach(f)
   }
 
+  override def remove(elem: A, acc: MySet[A]): MySet[A] = {
+    if (elem == head) tail
+    else tail.remove(elem, acc + head)
+  }
+  override def intersectionWith(anotherSet: MySet[A]): MySet[A] = {
+    flatMap { x =>
+      if (anotherSet.isEmpty) MySetEmpty()
+      else if (x == anotherSet.head) MySetEmpty() + x
+      else intersectionWith(anotherSet.tail)
+    }
+  }
+
+  override def differenceWith(anotherSet: MySet[A]): MySet[A] = ???
+}
+object MySet {
+  def apply[A](x: A*): MySet[A] = {
+    @tailrec
+    def build(seq: Seq[A], acc: MySet[A]): MySet[A] = {
+      if (seq.isEmpty) acc
+      else build(seq.tail, acc + seq.head)
+
+    }
+
+    build(x, new MySetEmpty[A])
+  }
+}
+object set extends App {
+
+
+
+  val mySet = MySet(1, 2, 3)
+  val myNewSet = MySet(2, 3, 5)
+  //  println(mySet)
+  //  println(mySet(4))
+  //  mySet + 3 flatMap (x => MySet(x, x * 10)) foreach println
+  val myIntercestedSet = mySet intersectionWith myNewSet
+  myIntercestedSet foreach println
 }
